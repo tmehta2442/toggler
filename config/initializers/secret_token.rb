ee4f23d46c44e6fdef9384ee110008f2def20472
tmehta2442 <tmehta@post.harvard.edu>
2013-11-29 11:59:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toggler::Application.config.secret_token = 'e84314b8f9fcd195dcb4c5335c8b2edca77a0d17f07dae5fe687896c8076d681dab10e9f427575312e8417f096ba92226ffe513ff7ebc21b1e046223db173c08'
