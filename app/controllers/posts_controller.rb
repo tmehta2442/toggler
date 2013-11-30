704ed25a4b8500e342835af76da3707666542290
tmehta2442 <tmehta@post.harvard.edu>
2013-11-29 17:33:43 -0800

    ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,31 @@
 class PostsController < ApplicationController
-
 	def new
+		@post = Post.new
 	end
 
 	def create
-		render text: params[:post].inspect
+		@post = Post.update(params[:post].permit(:title, :text))
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+
+		end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+
 end
